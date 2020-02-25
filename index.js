6dc8493c073cd4f247acd193bdda72bbbab297fb
= <=>
2020-02-25 18:13:46 +0700

    tach file update

--- index.js
+++ index.js
@@ -1,31 +1,7 @@
 /* eslint-disable prefer-template */
 /* eslint-disable eol-last */
 const express = require('express');
-const multer = require('multer');
-
-// cho phép lưu ở đâu , đặt tên file, và xử lí hàm
-const storage = multer.diskStorage({
-    destination: (req, file, cd) => cd(null, './public'),
-    filename: (req, file, cb) => cb(null, 'small-' + file.originalname)
-
-
-});
-
-// giới hạn kích thước file
-const limits = { fileSize: 1024000 };
-// dest : chỉ cho phép lưu ở đâu
-// const upload = multer({ dest: './public' });
-
-function fileFilter(req, file, cb) {
-    const { mimetype } = file;
-    if (mimetype === 'image/png' || mimetype === "'image/jpeg'") {
-        return cb(null, true);
-    }
-    cb(new Error('file khong hợp lệ'));
-}
-
-// chỉ truyền đươc 4 cái 
-const upload = multer({ storage, limits, fileFilter });
+const upload = require('./uploadConfig');
 
 const app = express();
 
@@ -35,7 +11,13 @@ app.set('view engine', 'ejs');
 app.get('/', (req, res) => res.render('home'));
 
 app.post('/signup', upload.single('profile'), (req, res) => {
-    res.send(req.body);
+    res.send(req.file);
 });
+
+app.use((err, req, res, next) => {
+    res.send(err.message);
+});
+// nếu không ghi đúng link thì sẽ tự động nhảy đến đây
+app.get('*', (req, res) => res.send('cannot find this link'));
 
 app.listen(3000, () => console.log('server start'));
---
+++ uploadConfig.js
@@ -0,0 +1,28 @@
+/* eslint-disable eol-last */
+const multer = require('multer');
+
+// cho phép lưu ở đâu , đặt tên file, và xử lí hàm
+const storage = multer.diskStorage({
+    destination: (req, file, cd) => cd(null, './public'),
+    filename: (req, file, cb) => cb(null, 'small-' + file.originalname)
+
+
+});
+
+// giới hạn kích thước file
+const limits = { fileSize: 1024000 };
+// dest : chỉ cho phép lưu ở đâu
+// const upload = multer({ dest: './public' });
+
+function fileFilter(req, file, cb) {
+    const { mimetype } = file;
+    if (mimetype === 'image/png' || mimetype === "'image/jpeg'") {
+        return cb(null, true);
+    }
+    cb(new Error('file khong hợp lệ'));
+}
+
+// chỉ truyền đươc 4 cái 
+const upload = multer({ storage, limits, fileFilter });
+
+module.exports = upload;
